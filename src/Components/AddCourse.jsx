0399eff05e30ff9c0557455fa2b59300c42643cd
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-11 23:08:08 +0530

    add course page done

---
+++ src/Components/AddCourse.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import React from 'react'
+
+const AddCourse = () => {const[data,changeData]=useState([
+    {
+        "title": "",
+        "description": "",
+        "startdate":"",
+        "duration": "",
+        "venue": "",
+        "trainer": ""
+    } ])
+     const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+     }
+     const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8080/add",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("could not add data")
+                }
+            }
+        ).catch().finally
+     }
+        return (
+            <div>
+                <Navbar />
+                <div className="container">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    
+                                <label htmlFor="" className="form-label">Ttitle</label>
+                                <input type="text" className="form-control" name='title' value={data.title} onChange={inputHandler}/>
+    
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    
+    
+                                <label htmlFor="" className="form-label">Description</label>
+                                <textarea name="description" id="" className="form-control" value={data.description} onChange={inputHandler}></textarea>
+    
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    
+    
+                                <label htmlFor="" className="form-label">Admission no</label>
+                                <input type="text" className="form-control" name='startdate' value={data.admNo} onChange={inputHandler}/>
+    
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    
+    
+                                <label htmlFor="" className="form-label">College</label>
+                                <input type="text" className="form-control" name='college' value={data.college} onChange={inputHandler}/>
+    
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    
+    
+    
+                            <button className="btn btn-success" onClick={readValue}>Add Student</button>                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+}
+
+export default AddCourse
